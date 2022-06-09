0e3504638bfb889184194fb8731f08b461431f17
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2022-06-09 09:30:53 +0200

    ✨ [Homepage] Make a clean and publishable home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,18 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/spots', name: 'spots')]
+    public function spots(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/general-rules', name: 'general_rules')]
+    public function generalRules(): Response
+    {
+        return $this->render('home/index.html.twig');
     }
 }
