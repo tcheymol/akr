fb1daea4874cd4c014a05b38c342b8dc10acd2ee
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2022-06-11 21:25:05 +0200

    ✨ [Texts] Add WIP message

--- src/Manager/TextManager.php
+++ src/Manager/TextManager.php
@@ -3,19 +3,22 @@
 namespace App\Manager;
 
 use App\Repository\TextRepository;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class TextManager
 {
     private TextRepository $repository;
+    private TranslatorInterface $translator;
 
-    public function __construct(TextRepository $repository)
+    public function __construct(TextRepository $repository, TranslatorInterface $translator)
     {
         $this->repository = $repository;
+        $this->translator = $translator;
     }
 
     public function getContent(string $key): ?string {
         $text = $this->repository->findOneBy(['key' => $key]);
 
-        return $text?->getContent();
+        return $text?->getContent() ?? $this->translator->trans('section_in_progress');
     }
 }
