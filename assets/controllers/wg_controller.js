834ab6739493779bb2993e5d8a5c84061039a10b
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2022-07-14 14:30:04 +0200

    ✨ [Spots] Add wg widget on spots list

--- assets/controllers/wg_controller.js
+++ assets/controllers/wg_controller.js
@@ -6,17 +6,21 @@ import { Controller } from '@hotwired/stimulus';
 */
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
-    static targets = ['container']
-    static values = {
-        id: Number,
-    }
+  static targets = ['container']
+  static values = {
+    id: Number,
+    wgId: Number,
+  }
 
-    connect() {
-        var arg = [`s=${this.idValue}`, "m=100", "uid=wg_fwdg_216360_100_1655040885667", "wj=knots", "tj=c", "waj=m", "odh=0", "doh=24", "fhours=240", "hrsm=2", "vt=forecasts", "lng=en", "idbs=1", "p=WINDSPD,GUST,SMER,TMP,FLHGT,CDC,APCP1s,RATING"];
-        var script = document.createElement("script");
-        script.id = "wg_fwdg_216360_100_1655040885667"
-        script.src = "https://www.windguru.cz/js/widget.php?" + (arg.join("&"));
+  connect() {
+    const scriptId = `wg_fwdg_${this.idValue}`;
+    const arg = [`s=${this.wgIdValue}`, "m=100", `uid=${scriptId}`, "wj=knots", "tj=c", "waj=m", "odh=0", "doh=24", "fhours=240", "hrsm=2", "vt=forecasts", "lng=en", "idbs=1", "p=WINDSPD,GUST,SMER,TMP,FLHGT,CDC,APCP1s,RATING"];
+    const script = document.createElement("script");
+    script.id = scriptId
+    script.src = "https://www.windguru.cz/js/widget.php?" + (arg.join("&"));
 
-        this.containerTarget.appendChild(script);
-    }
+    console.log(this.containerTarget);
+
+    this.containerTarget.appendChild(script);
+  }
 }
